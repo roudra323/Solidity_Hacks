edc54addab6efd92a2cbbdd675f70f21d98f7771
Asir Shahriar Roudra <asirroudra@gmail.com>
2023-07-08 14:43:19 +0600

    Update unsafeDeleCall01.sol

--- unsafeDeleCall01.sol
+++ unsafeDeleCall01.sol
@@ -1,1 +1,37 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract A {
+    address public immutable owner; //use immutable to avoid delegate call attacks
+    B public b;
+
+    constructor(B _b) {
+        owner = msg.sender;
+        b = _b;
+    }
+
+    fallback() external payable {
+        address(b).delegatecall(msg.data);
+    }
+}
+
+contract B {
+    address public owner;
+
+    function setOwner() public {
+        owner = msg.sender;
+    }
+}
+
+contract C {
+    address public contractAddr;
+
+    constructor(address _contractAddr){
+        contractAddr = _contractAddr;
+    }
+
+    function attack() public{
+        contractAddr.call(abi.encodeWithSignature("setOwner()"));
+    }
+}
